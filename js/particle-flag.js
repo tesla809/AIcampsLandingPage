272a537ee205cc474dd35845d9c46095f138a4ec
Anthony Albertorio <anthony.albertorio@gmail.com>
2016-07-03 17:47:56 -0400

    Anthony Albertorio added initial project files for AI camps website

---
+++ js/particle-flag.js
@@ -0,0 +1,35 @@
+//particle flag
+
+var png = new Image();
+png.onload = drawScene;
+png.src = "../flag-icon-css/flags/1x1/un.svg";
+
+var canvas    = document.getElementById("scene");
+var ctx       = canvas.getContext("2d");
+
+canvas.width = png.width;
+canvas.height = png.height;
+
+ctx.drawImage(png, 0, 0);
+var data = ctx.getImageData(0, 0, png.width, png.height);
+ctx.clearRect(0,0,canvas.width, canvas.height);
+
+
+var particles = [];
+for (var y = 0, y2 = data.height; y < y2; y++) {
+    for (var x = 0, x2 = data.width; x < x2; x++) {
+        if (data.data[(x * 4 + y * 4 * data.width) + 3] > 128) {
+            var particle = {
+                x : x,
+                y : y
+            };
+            particles.push(particle);
+        }
+    }
+}
+
+ctx.fillStyle = "white";
+for(var i=0, j=particles.length;i<j;i++){
+    var particle = particles[i];
+    ctx.fillRect(particle.x, particle.y, 1, 1);
+}
---
+++ js/playlist.js
@@ -0,0 +1,6 @@
+var lastDay = new Howl({
+  urls: [],
+  autoplay: true,
+  loop: true,
+  volume: 0.5,
+}).play();
